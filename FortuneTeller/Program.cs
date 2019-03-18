7b46fa91264dd0b5a5e92961300ac378af0b6f8b
Jessie Monson <jessiemonson1980@gmail.com>
2019-03-17 19:39:04 -0500

    final code

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -6,9 +6,33 @@ namespace NextBirthday
     {
         public static void Main(string[] args)
         {
+            Console.Write("Enter your birthdate: ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            string birthDayOfWeek = NextBirthdayDay(birthDate);
+            Console.WriteLine(birthDayOfWeek);
             Console.ReadLine();
         }
 
         // TODO: Create a method that takes the date someone was born and returns the name of the day on their next birthday.
+        public static string NextBirthdayDay(DateTime birthDate)
+        {
+            int birthMonth = birthDate.Month;
+            int birthDay = birthDate.Day;
+            DateTime birthdayThisYear = DateTime.Parse(birthMonth + "/" + birthDay + "/2019");
+
+            DateTime nextBirthday;
+            //If next birthdate will be this year
+            if (birthdayThisYear.CompareTo(DateTime.Today) > 0)
+            {
+                nextBirthday = DateTime.Parse(birthMonth + "/" + birthDay + "/2019");
+            }
+            else
+            {
+                //If next birthdate will be next year
+                nextBirthday = DateTime.Parse(birthMonth + "/" + birthDay + "/2020");
+            }
+            return nextBirthday.DayOfWeek.ToString();
+        }
     }
 }
